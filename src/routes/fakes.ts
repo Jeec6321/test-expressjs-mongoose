a74f2cb063397b7af4ae1a35c3d416a6e069d690
Esteban <juanestebanescobarc@gmail.com>
2022-09-12 18:01:57 -0500

    remove array and fix lumens

--- src/routes/fakes.ts
+++ src/routes/fakes.ts
@@ -93,7 +93,7 @@ routes.get("/station/:id", async (req, res) => {
       temperature: getFakeValue(Math.random() * (60 * max - 60 * min) + 60 * min, 60),
       humidity: getFakeValue(Math.random() * (100 * max - 100 * min) + 100 * min, 100),
       precipitation: getFakeValue(Math.random() * (10000 * max - 10000 * min) + 10000 * min, 10000),
-      lumens: getFakeValue(Math.random() * (300000 * max - 3000000 * min) + 3000000 * min, 300000),
+      lumens: getFakeValue(Math.random() * (300000 * max - 300000 * min) + 300000 * min, 300000),
       rain: getFakeValue(Math.random() * (1000 * max - 1000 * min) + 1000 * min, 1000),
       direction: getFakeValue(Math.random() * (360 * max - 360 * min) + 360 * min, 360),
       uv: getFakeValue(Math.random() * (15 * max - 15 * min) + 15 * min, 15),
@@ -124,19 +124,19 @@ routes.get("/sensor-history", async (req, res) => {
 });
 
 function getFakeValue(valueIn: number, max: number) {
-  var object = [{
-    value: valueIn,
+  var object = {
+    value: parseFloat(valueIn.toFixed(2)),
     color: "",
-  }];
+  };
 
-  if (object[0].value < max * 0.5) {
-    object[0].color = "34a853";
-  } else if (object[0].value < max*0.7 && object[0].value > max*0.51) {
-    object[0].color = "fbbc05";
-  } else if (object[0].value < max*0.90 && object[0].value > max*0.71) {
-    object[0].color = "fa5901";
-  } else if (object[0].value > max*0.91) {
-    object[0].color = "FF0000";
+  if (object.value < max * 0.5) {
+    object.color = "34a853";
+  } else if (object.value < max*0.7 && object.value > max*0.51) {
+    object.color = "fbbc05";
+  } else if (object.value < max*0.90 && object.value > max*0.71) {
+    object.color = "fa5901";
+  } else if (object.value > max*0.91) {
+    object.color = "FF0000";
   }
 
   return object;
